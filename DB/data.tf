7736321cef6fb11ba85554bda2d0c8b5a7816ac6
Adarsh <adarshhp14@gmail.com>
2021-11-17 17:38:41 +0530

    data.tf

---
+++ DB/data.tf
@@ -0,0 +1,21 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-b60"
+    key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = var.ENV
+}
+
+data "aws_secretsmanager_secret_version" "secrets-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+resource "local_file" "foo" {
+  content  = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
+  filename = "/tmp/1"
+}
